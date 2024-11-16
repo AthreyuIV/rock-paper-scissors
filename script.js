729e78fbc2c68ec7492282226ef4a649701cc124
AthreyuIV <182107666+AthreyuIV@users.noreply.github.com>
2024-11-16 08:38:45 -0800

    Push new changes
    Created a loop that's working but need to figure out a way
    to address it infinetly looping in the case of a tie game and
    to start each round off by using new values for humanSelection
    and computerSelection

--- script.js
+++ script.js
@@ -36,74 +36,69 @@ function getHumanChoice() {
     }
     // ELSE "You'll never be Chuck Norris with that attitude."
     else {
-        console.log("You'll never be Chuck Norris with that attitude.")
+        var tieGame = console.log("You'll never be Chuck Norris with that attitude.")
+        console.log(tieGame);
     }
 }
 
 //_______________________________________________________
 
-// create a CONST called humanScore with a value of 0
-let humanScore = 0
-//create a CONST called computerScore with a value of 0
-let computerScore = 0
+function playGame(numRounds) {
+    let humanScore = 0;
+    let computerScore = 0;
+    // Create a FUNCTION named playRound (humanChoice, computerChoice)
+    function playRound(humanChoice, computerChoice) {
+        // IF humanSelection === computerSelection
+        if (humanSelection === computerSelection) {
+            //CONSOLE.LOG "A tie is like a hug, it's nice, but it doesn't really solve anything."
+            console.log("A tie is like a hug, it's nice, but it doesn't really solve anything.");
+        }
+        // ELSE IF humanSelection === "rock" &&  computerSelection === "paper"
+        else if (humanSelection === "rock" && computerSelection === "paper") {
+            //CONSOLE.LOG "You lose dude!"
+            computerScore++;
+            console.log("You lose dude, " + computerSelection + " beats " + humanSelection + ".");
+        }
+        // ELSE IF humanSelection === "paper" && computerSelection === "scissors"
+        else if (humanSelection === "paper" && computerSelection === "scissors") {
+            //CONSOLE.LOG "You lose dude!"
+            computerScore++;
+            console.log("You lose dude, " + computerSelection + " beats " + humanSelection + ".");
+        }
+        // ELSE IF humanSelection === "scissors" && computerSelection === "rock"
+        else if (humanSelection === "scissors" && computerSelection === "rock") {
+            //CONSOLE.LOG "You lose dude!"
+            computerScore++;
+            console.log("You lose dude, " + computerSelection + " beats " + humanSelection + ".");
+        }
+        // ELSE IF humanSelection === "rock" && computerSelection ==="scissors"
+        else if (humanSelection === "rock" && computerSelection === "scissors") {
+            //CONSOLE.LOG "You win dude!"
+            humanScore++;
+            console.log("You win dude, " + humanSelection + " beats " + computerSelection + ".");
+        }
+        // ELSE IF humanSelection === "paper" && computerSelection === "rock"
+        else if (humanSelection === "paper" && computerSelection === "rock") {
+            //CONSOLE.LOG "You win dude!"
+            humanScore++;
+            console.log("You win dude, " + humanSelection + " beats " + computerSelection + ".");
+        }
+        // ELSE IF humanSelection === "scissors" && computerSelection === "paper"
+        else if (humanSelection === "scissors" && computerSelection === "paper") {
+            //CONSOLE.LOG "You win dude!"
+            humanScore++;
+            console.log("You win dude, " + humanSelection + " beats " + computerSelection + ".");
+        }
+    }
 
-//_______________________________________________________
-
-// Create a FUNCTION named playRound (humanChoice, computerChoice)
-function playRound(humanChoice, computerChoice) {
-    // IF humanSelection === computerSelection
-    if (humanSelection === computerSelection) {
-        //CONSOLE.LOG "A tie is like a hug, it's nice, but it doesn't really solve anything."
-        console.log("A tie is like a hug, it's nice, but it doesn't really solve anything.");
-    }
-    // ELSE IF humanSelection === "rock" &&  computerSelection === "paper"
-    else if (humanSelection === "rock" && computerSelection === "paper") {
-        //CONSOLE.LOG "You lose dude!"
-        computerScore++;
-        console.log("You lose dude, " + computerSelection + " beats " + humanSelection + ".");
-    }
-    // ELSE IF humanSelection === "paper" && computerSelection === "scissors"
-    else if (humanSelection === "paper" && computerSelection ==="scissors") {
-        //CONSOLE.LOG "You lose dude!"
-        computerScore++;
-        console.log("You lose dude, " + computerSelection + " beats " + humanSelection + ".");
-    }
-    // ELSE IF humanSelection === "scissors" && computerSelection === "rock"
-    else if (humanSelection === "scissors" && computerSelection === "rock") {
-        //CONSOLE.LOG "You lose dude!"
-        computerScore++;
-        console.log("You lose dude, " + computerSelection + " beats " + humanSelection + ".");
-    }
-    // ELSE IF humanSelection === "rock" && computerSelection ==="scissors"
-    else if (humanSelection === "rock" && computerSelection === "scissors") {
-        //CONSOLE.LOG "You win dude!"
-        humanScore++;
-        console.log("You win dude, " + humanSelection + " beats " + computerSelection + ".");
-    }
-    // ELSE IF humanSelection === "paper" && computerSelection === "rock"
-    else if (humanSelection === "paper" && computerSelection === "rock") {
-        //CONSOLE.LOG "You win dude!"
-        humanScore++;
-        console.log("You win dude, " + humanSelection + " beats " + computerSelection + ".");
-    }    
-    // ELSE IF humanSelection === "scissors" && computerSelection === "paper"
-    else if (humanSelection === "scissors" && computerSelection === "paper") {
-        //CONSOLE.LOG "You win dude!"
-        humanScore++;
-        console.log("You win dude, " + humanSelection + " beats " + computerSelection + ".");
+    while (humanScore < 5 && computerScore < 5) {
+        playRound(humanSelection, computerSelection);
     }
 }
 
-// // GLOBAL CONST humanSelection with a value getHumanChoice();
+//_____________________________________________________________
+
 const humanSelection = getHumanChoice();
-
-// // GLOBAL CONST computerSelection with a value getComputerChoice();
 const computerSelection = getComputerChoice();
 
-// // Call FUNCTION playRound with arguements humanSelection and computerSelection
-playRound(humanSelection, computerSelection);
-
-
-
-
-
+playGame();
